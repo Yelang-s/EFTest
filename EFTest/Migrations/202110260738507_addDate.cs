3b42c2c91d0e38f1ec3266cd0e54936cb61eb017
yelang.qin <yelang.qin@A1400>
2021-10-26 15:59:41 +0800

    r

---
+++ EFTest/Migrations/202110260738507_addDate.cs
@@ -0,0 +1,18 @@
+﻿namespace EFTest.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addDate : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.ProductInfo", "Date", c => c.DateTime(nullable: false, storeType: "date"));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.ProductInfo", "Date");
+        }
+    }
+}
--- EFTest/Models/ProductInfo.cs
+++ EFTest/Models/ProductInfo.cs
@@ -4,6 +4,7 @@
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
+    using System.Drawing;
 
     [Table("ProductInfo")]
     class ProductInfo
@@ -26,6 +27,8 @@
 
         public bool T3Result { get; set; }
 
+        [Column(TypeName = "date")]
+        public DateTime Date { get; set; }
         public DateTime StartTime { get; set; }
 
         public DateTime EndTime { get; set; }
